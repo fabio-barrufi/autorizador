0e9375399d20a86cbb6ef7d8914caaba59be370d
Fábio Barrufi Pagano <61845465+fabio-barrufi@users.noreply.github.com>
2024-06-14 09:57:49 -0300

    Adicionando repository

---
+++ src/main/java/com/miniautorizador/data/database/repository/CartoesRepository.java
@@ -0,0 +1,14 @@
+package com.miniautorizador.data.database.repository;
+
+import com.miniautorizador.data.models.CartaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartoesRepository extends JpaRepository<CartaoModel, String> {
+    Optional<CartaoModel> findByNumeroCartao(String numeroCartao);
+
+    Boolean existsByNumeroCartao(String numeroCartao);
+}
