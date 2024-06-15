3395adf27091c8602305266c42f6758eb7567961
Fábio Barrufi Pagano <61845465+fabio-barrufi@users.noreply.github.com>
2024-06-15 18:08:41 -0300

    Adicionando builder no CartaoResponseDTO

--- src/main/java/com/miniautorizador/data/dtos/response/CartaoResponseDTO.java
+++ src/main/java/com/miniautorizador/data/dtos/response/CartaoResponseDTO.java
@@ -2,12 +2,14 @@ package com.miniautorizador.data.dtos.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CartaoResponseDTO {
     @Schema(description = "Número do cartão do usuário.", example = "6549873025634501")
     private String numeroCartao;
