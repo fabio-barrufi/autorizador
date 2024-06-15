9c3c0317c5414fc17c44c0b6a95a9ff048949fc7
Fábio Barrufi Pagano <61845465+fabio-barrufi@users.noreply.github.com>
2024-06-15 18:11:58 -0300

    Testes unitários controller de cartões

---
+++ src/test/java/com/miniautorizador/entrypoint/controller/CartoesControllerTest.java
@@ -0,0 +1,101 @@
+package com.miniautorizador.entrypoint.controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.miniautorizador.data.dtos.request.CartaoRequestDTO;
+import com.miniautorizador.domain.usecase.CartoesUseCase;
+import helpers.CartaoMock;
+import com.miniautorizador.entrypoint.controller.impl.CartoesControllerImpl;
+import com.miniautorizador.infrastructure.ApiConstants;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+class CartoesControllerTest {
+
+    @InjectMocks
+    CartoesControllerImpl controller;
+
+    @Mock
+    CartoesUseCase useCase;
+
+    private MockMvc mockMvc;
+
+    private Gson gson;
+
+    private final String numeroCartao = "6549873025634503";
+
+    private final CartaoRequestDTO cartaoRequestDTO = CartaoMock.cartaoRequestDTOMock();
+
+    private final String baseUrl = ApiConstants.CARTOES_BASE_URL;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        this.gson = new GsonBuilder()
+                .create();
+    }
+
+    @Test
+    void criarCartao() throws Exception {
+        var mvcResult =
+                mockMvc
+                        .perform(
+                         post(baseUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .content(gson.toJson(cartaoRequestDTO)))
+                        .andExpect(status().is(HttpStatus.CREATED.value()))
+                        .andReturn();
+
+        Assertions.assertThat(mvcResult.getResponse()).isNotNull();
+        Assertions.assertThat(mvcResult.getResponse().getStatus()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void consultarSaldoCartao() throws Exception {
+        String path = baseUrl + "/" + numeroCartao;
+
+        var mvcResult = mockMvc.perform(
+                        get(path)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+        Assertions.assertThat(mvcResult.getResponse()).isNotNull();
+        Assertions.assertThat(mvcResult.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    void validarCriarCartao(){
+        controller.criarCartao(cartaoRequestDTO);
+
+        verify(useCase, times(1)).criarCartao(cartaoRequestDTO);
+    }
+
+    @Test
+    void validarConsultarSaldoCartao(){
+        controller.consultarSaldoCartao(numeroCartao);
+
+        verify(useCase, times(1)).consultarSaldoCartao(numeroCartao);
+    }
+}
---
+++ src/test/java/com/miniautorizador/entrypoint/controller/impl/CartoesControllerImplTest.java
@@ -0,0 +1,116 @@
+package com.miniautorizador.entrypoint.controller.impl;
+
+import com.miniautorizador.data.dtos.request.CartaoRequestDTO;
+import com.miniautorizador.data.dtos.response.CartaoResponseDTO;
+import com.miniautorizador.data.exceptions.CampoInvalidoException;
+import com.miniautorizador.data.exceptions.CartaoExistenteException;
+import com.miniautorizador.data.exceptions.CartaoInexistenteException;
+import com.miniautorizador.data.mapper.CartoesMapper;
+import com.miniautorizador.domain.usecase.CartoesUseCase;
+import helpers.CartaoMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+class CartoesControllerImplTest {
+
+    @Mock
+    CartoesUseCase useCase;
+
+    @InjectMocks
+    private CartoesControllerImpl controller;
+
+    @Mock
+    CartoesMapper mapper;
+
+    private final String numeroCartao = "6549873025634503";
+    private final String numeroCartaoInvalido = "abc";
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        controller = new CartoesControllerImpl(useCase, mapper);
+    }
+
+    @Test
+    void criarCartaoComSucesso() {
+        CartaoRequestDTO cartaoRequestDTO = CartaoMock.cartaoRequestDTOMock();
+        CartaoResponseDTO cartaoResponseDTO = CartaoMock.cartaoResponseDTOMock();
+
+        when(useCase.criarCartao(any(CartaoRequestDTO.class))).thenReturn(cartaoResponseDTO);
+
+        ResponseEntity<CartaoResponseDTO> response = controller.criarCartao(cartaoRequestDTO);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(cartaoResponseDTO, response.getBody());
+    }
+
+    @Test
+    void criarCartaoThrowsCampoInvalidoException() {
+        CartaoRequestDTO cartaoRequestDTO = CartaoMock.cartaoRequestDTOMock();
+        cartaoRequestDTO.setNumeroCartao(numeroCartaoInvalido);
+
+        assertThrows(CampoInvalidoException.class, () -> {
+            controller.criarCartao(cartaoRequestDTO);
+        });
+    }
+
+    @Test
+    void criarCartaoThrowsCartaoExistenteException() {
+        CartaoRequestDTO cartaoRequestDTO = CartaoMock.cartaoRequestDTOMock();
+        CartaoResponseDTO cartaoResponseDTO = CartaoMock.cartaoResponseDTOMock();
+
+        doThrow(CartaoExistenteException.class).when(useCase).criarCartao(any(CartaoRequestDTO.class));
+
+        when(mapper.requestToResponseDTO(any(CartaoRequestDTO.class))).thenReturn(cartaoResponseDTO);
+
+        ResponseEntity<CartaoResponseDTO> response = controller.criarCartao(cartaoRequestDTO);
+
+        assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(cartaoResponseDTO, response.getBody());
+    }
+
+    @Test
+    void consultarSaldoCartaoComSucesso() {
+        BigDecimal saldo = BigDecimal.valueOf(1000);
+
+        when(useCase.consultarSaldoCartao(numeroCartao)).thenReturn(saldo);
+
+        ResponseEntity<BigDecimal> response = controller.consultarSaldoCartao(numeroCartao);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(saldo, response.getBody());
+    }
+
+    @Test
+    void consultarSaldoCartaoThrowsCampoInvalidoException() {
+        assertThrows(CampoInvalidoException.class, () -> {
+            controller.consultarSaldoCartao(numeroCartaoInvalido);
+        });
+    }
+
+    @Test
+    void consultarSaldoCartaoThrowsCartaoInexistenteException() {
+        doThrow(CartaoInexistenteException.class).when(useCase).consultarSaldoCartao(numeroCartao);
+
+        ResponseEntity<BigDecimal> response = controller.consultarSaldoCartao(numeroCartao);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+}
