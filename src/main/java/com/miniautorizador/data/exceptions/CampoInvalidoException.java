da01946b7ed9dd5ebfd5c10407fe0842d21cb6ce
Fábio Barrufi Pagano <61845465+fabio-barrufi@users.noreply.github.com>
2024-06-14 21:20:30 -0300

    Adicionando status bad request p/ exception campo inválido

--- src/main/java/com/miniautorizador/data/exceptions/CampoInvalidoException.java
+++ src/main/java/com/miniautorizador/data/exceptions/CampoInvalidoException.java
@@ -1,5 +1,9 @@
 package com.miniautorizador.data.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CampoInvalidoException extends RuntimeException {
 
     public CampoInvalidoException(String message) {
